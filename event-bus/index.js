b44d3825a67f7581ca3c804b9afebf3195d430f4
Akshay Sutar <akshay.sutar5@outlook.com>
2021-10-25 17:53:05 +0530

    added kubernetes

--- event-bus/index.js
+++ event-bus/index.js
@@ -9,10 +9,10 @@ app.post("/events", async (req, res) => {
   console.log("event received", event);
 
   try {
-    await axios.post("http://localhost:4000/events", event);
-    await axios.post("http://localhost:4001/events", event);
-    await axios.post("http://localhost:4002/events", event);
-    await axios.post("http://localhost:4003/events", event);
+    await axios.post("http://posts-clusterip-srv:4000/events", event);
+    await axios.post("http://comments-srv:4001/events", event);
+    await axios.post("http://query-srv:4002/events", event);
+    await axios.post("http://moderation-srv:4003/events", event);
     console.log("data");
   } catch (err) {
     console.log(err);
